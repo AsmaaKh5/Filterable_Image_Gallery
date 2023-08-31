af2f8fae74193bbcc62df15b8de68b8fcfe1c862
AsmaaKh5 <AsmaaKhaled.55@gmail.com>
2023-08-31 20:03:16 +0300

    Page

---
+++ script.js
@@ -0,0 +1,57 @@
+/*
+
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll(".filterable_cards .cards");
+
+const filterCards = (e) =>{
+    document.querySelector(" .filter_buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+   // console.log(e.target);
+
+   filterableCards.forEach(cards =>{
+    cards.classList.add("hide");
+    if(cards.dataset.name === e.target.dataset.filter ||e.target.dataset.filter === " all" ){
+       return  cards.classList.replace("hide" , "show");
+ 
+    }
+    cards.classList.add("hide");
+
+
+   });
+};
+
+
+// console.log(filterButtons, filterCards);
+
+filterButtons.forEach(button => button.addEventListener("Click",filterCards ));
+
+
+*/
+
+
+
+
+
+
+
+
+
+// Select relevant HTML elements
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll(".filterable_cards .cards");
+
+// Function to filter cards based on filter buttons
+const filterCards = (e) => {
+    document.querySelector(".filter_buttons .active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(cards => {
+        // show the card if it matches the clicked filter or show all cards if "all" filter is clicked
+        if(cards.dataset.name === e.target.dataset.filter || e.target.dataset.filter === "all") {
+            return cards.classList.replace("hide", "show");
+        }
+        cards.classList.add("hide");
+    });
+}
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
